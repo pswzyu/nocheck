ccc903cf61435a5cd6b310344e48b58ed13d4e66
PSWZ-ZhangY <pswzyu@pswzyu-cp-ubuntu.(none)>
2014-09-24 13:47:04 -0400

    fix empty line in json response

--- api/v1/api.php
+++ api/v1/api.php
@@ -7,7 +7,6 @@ include_once(FROOT."api/v1/api.class.php");
 if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
     $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
 }
-
 try {
     $API = new MyAPI($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
     $API->setDBConnection($udb);
