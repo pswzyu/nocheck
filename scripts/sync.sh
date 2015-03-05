26f828733466dc18f198f5b7a29fbd006310549d
pswzyu <pswzyu@gmail.com>
2015-03-05 09:04:59 -0500

    add sync script for linux crontab

---
+++ scripts/sync.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+TMP_DIR=/var/www/html/nocheck/scripts
+ROOT_DIR=/var/www/html/nocheck
+
+if [ -f "$TMP_DIR/$1.lock" ]; then
+	echo "######`date`##### 上次未完成--" >> $TMP_DIR/$1.log
+else
+	touch $TMP_DIR/$1.lock
+	echo "######`date`##### 上次已完成--" >> $TMP_DIR/$1.log
+	php $ROOT_DIR/sync.php $1
+	echo "--$?--" >> $TMP_DIR/$1.log
+	rm $REPO_SYNC/$1.lock
+fi
+
